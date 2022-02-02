093f5673f53109853c854d5bac4fc51b7ce22d03
pseregiet <patryk.seregiet@gmail.com>
2022-02-02 22:40:02 +0100

    store md5_model and md5_anim in the kh map

--- md5loader.c
+++ md5loader.c
@@ -7,18 +7,16 @@
 #include <assert.h>
 #include <stdio.h>
 
-KHASH_MAP_INIT_STR(animmap, struct md5_anim *)
-KHASH_MAP_INIT_STR(modelmap, struct md5_model *)
+KHASH_MAP_INIT_STR(modelmap, struct md5_model)
+KHASH_MAP_INIT_STR(animmap, struct md5_anim)
 
 static struct animations {
     struct growing_alloc names;
-    struct growing_alloc anims;
     khash_t(animmap) map;
 } animations;
 
 static struct models {
     struct growing_alloc names;
-    struct growing_alloc models;
     khash_t(modelmap) map;
 } models;
 
@@ -40,41 +38,34 @@ inline static char *addname(struct growing_alloc *alloc, const char *name)
 static struct md5_model *model_append(const char *fp, const char *name)
 {
     struct md5_model mdl;
-
     if (md5model_open(fp, &mdl))
         return 0;
 
-    struct md5_model *mdldst = (struct md5_model *)growing_alloc_get(&models.models, sizeof(struct md5_model));
-    assert(mdldst);
-    memcpy(mdldst, &mdl, sizeof(mdl));
     char *newname = addname(&models.names, name);
-
     int ret;
     khint_t idx = kh_put(modelmap, &models.map, newname, &ret);
-    kh_value(&models.map, idx) = mdldst;
-
-    return mdldst;
+    kh_value(&models.map, idx) = mdl;
+    return &kh_value(&models.map, idx);
 }
 
-static struct md5_anim *animation_append(const char *fp, const char *name)
+static khint_t animation_append(const char *fp, const char *name)
 {
     struct md5_anim anim;
-
     if (md5anim_open(fp, &anim))
         return 0;
 
-    struct md5_anim *animdst = (struct md5_anim *)growing_alloc_get(&animations.anims, sizeof(struct md5_anim));
-    assert(animdst);
-    memcpy(animdst, &anim, sizeof(anim));
     char *newname = addname(&models.names, name);
-
     int ret;
     khint_t idx = kh_put(animmap, &animations.map, newname, &ret);
-    kh_value(&animations.map, idx) = animdst;
+    kh_value(&animations.map, idx) = anim;
 
-    return animdst;
+    return idx;
 }
 
+union aniunio{
+    struct md5_anim *a;
+    khint_t i;
+};
 static int load_model_anims(const char* model, const char **animfps, int ac)
 {
     if (!model || !animfps)
@@ -93,23 +84,40 @@ static int load_model_anims(const char* model, const char **animfps, int ac)
     if (!mdl)
         return -1;
 
-    struct md5_anim **anims = calloc(ac, sizeof(*anims));
+    union aniunio *anims = calloc(ac, sizeof(union aniunio));
     for (int i = 0; i < ac; ++i) {
         idx = kh_get(animmap, &animations.map, animfps[i]);
         if (idx != kh_end(&animations.map)) {
-            anims[i] = kh_val(&animations.map, idx);
+            anims[i].i = idx;
             continue;
         }
 
         snprintf(fp, 0x1000, "md5/models/%s.md5anim", animfps[i]);
-        anims[i] = animation_append(fp, animfps[i]);
-        if (!anims[i])
+        anims[i].i = animation_append(fp, animfps[i]);
+        if (anims[i].i == kh_end(&animations.map))
             return -1;
     }
-    mdl->anims = anims;
+    mdl->anims = (struct md5_anim **)anims;
     mdl->acount = ac;
 
     return 0;
+}
+
+static void fixup_animations_pointers() {
+    khint_t mi;
+
+    for (mi = kh_begin(&models.map); mi != kh_end(&models.map); ++mi) {
+        if (!kh_exist(&models.map, mi))
+            continue;
+
+        struct md5_model *mdl = &kh_val(&models.map, mi);
+        for (int i = 0; i < mdl->acount; ++i) {
+            union aniunio au = {
+                .a = mdl->anims[i]
+            };
+            mdl->anims[i] = &kh_val(&animations.map, au.i);
+        }
+    }
 }
 
 static int md5models_json() {
@@ -158,6 +166,8 @@ static int md5models_json() {
             goto freejson;
     }
 
+    fixup_animations_pointers();
+
     ret = 0;
 freejson:
     cJSON_Delete(json);
@@ -172,7 +182,7 @@ struct md5_model *md5loader_find(const char *name)
     if (idx == kh_end(&models.map))
         return 0;
 
-    return kh_val(&models.map, idx);
+    return &kh_val(&models.map, idx);
 }
 
 int md5loader_init()
@@ -180,9 +190,7 @@ int md5loader_init()
     const int ms = sizeof(struct md5_model);
     const int as = sizeof(struct md5_anim);
     assert(!growing_alloc_init(&models.names, 0, 1));
-    assert(!growing_alloc_init(&models.models, ms * MODELS_PER_POOL, MODELS_POOL_COUNT));
     assert(!growing_alloc_init(&animations.names, 0, 1));
-    assert(!growing_alloc_init(&animations.anims, as * ANIMS_PER_POOL, ANIMS_POOL_COUNT));
     return md5models_json();
 }
 
@@ -193,21 +201,19 @@ void md5loader_kill()
         if (!kh_exist(&models.map, i))
             continue;
 
-        md5model_kill(kh_val(&models.map, i));
+        md5model_kill(&kh_val(&models.map, i));
     }
 
     for (i = kh_begin(&animations.map); i != kh_end(&animations.map); ++i) {
         if (!kh_exist(&animations.map, i))
             continue;
 
-        md5anim_kill(kh_val(&animations.map, i));
+        md5anim_kill(&kh_val(&animations.map, i));
     }
     kh_destroy(modelmap, &models.map);
     kh_destroy(animmap, &animations.map);
 
     growing_alloc_kill(&models.names);
-    growing_alloc_kill(&models.models);
     growing_alloc_kill(&animations.names);
-    growing_alloc_kill(&animations.anims);
 }
 
